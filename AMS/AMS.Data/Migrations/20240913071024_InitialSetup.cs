8a07d1bce9d71af68ab8b65e7d88aa61dac42882
dilawarbukhari <syedbadshahalinaqvi@gmail.com>
2024-09-13 12:10:49 +0500

    add migration

---
+++ AMS/AMS.Data/Migrations/20240913071024_InitialSetup.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AMS.DATA.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialSetup : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "HaveValidTest",
+                schema: "Domain",
+                table: "ApplicationForm",
+                type: "bit",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(bool),
+                oldType: "bit");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "HaveValidTest",
+                schema: "Domain",
+                table: "ApplicationForm",
+                type: "bit",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldType: "bit",
+                oldDefaultValue: false);
+        }
+    }
+}
